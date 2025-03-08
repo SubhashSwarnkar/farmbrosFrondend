0fd9132dc69638f85150c814ac2aabec6bbf38ae
SubhashSwarnkar <subsoni10@gmail.com>
2025-03-08 13:34:09 +0530

    rema

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://farmbros-obhk.onrender.com/api/auth/login", { email, password });
+      const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
 
       // ✅ Save token and user ID in localStorage
       localStorage.setItem("token", res.data.token);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,7 +19,7 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://farmbros-obhk.onrender.com/api/auth/register", {
+      const res = await axios.post("http://localhost:5000/api/auth/register", {
         name,
         email,
         phone,
